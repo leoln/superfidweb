acc25c26d07e0ae237f075907924b29d68edf68f
Leonardo Lenin Firmino de Melo <leonardoln@outlook.com>
2017-10-11 19:25:35 -0300

    Deploy 1

--- src/main/java/br/com/bhl/superfid/controller/HomeController.java
+++ src/main/java/br/com/bhl/superfid/controller/HomeController.java
@@ -5,13 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/index")
+@RequestMapping("superfidweb.herokuapp.com")
 public class HomeController
 {
 
    @GetMapping
    public String index() {
-      return "home/index";
+      return "index/index";
    }
    
 }
