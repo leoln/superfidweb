b8e240a233f6e4fa8d7f90863f94592c25b9fa9d
Leonardo Lenin Firmino de Melo <leonardoln@outlook.com>
2017-10-16 00:42:01 -0200

    json 4546

--- src/main/java/br/com/bhl/superfid/controller/UsuarioJsonController.java
+++ src/main/java/br/com/bhl/superfid/controller/UsuarioJsonController.java
@@ -3,6 +3,7 @@ package br.com.bhl.superfid.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -30,12 +31,12 @@ public class UsuarioJsonController {
 		return usuarioJson;
 	}
 	
-	@RequestMapping(value = "/cadastrar", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	public void usuarioCadastro(String json) {
+	@RequestMapping(value = "/cadastrar", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)	
+	public void usuarioCadastro( @RequestBody String json) {
 		Usuario usuario = gson.fromJson(json, Usuario.class);
 		System.out.println(usuario);
 		usuarioService.addUsuario(usuario);
+		
 	}
 	
 }
