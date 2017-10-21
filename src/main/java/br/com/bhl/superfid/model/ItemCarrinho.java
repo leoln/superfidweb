e1bdbb48db95b22ec15a2128bab181911acac95f
Leonardo Lenin Firmino de Melo <leonardoln@outlook.com>
2017-10-21 13:54:19 -0200

    teste

--- src/main/java/br/com/bhl/superfid/model/ItemCarrinho.java
+++ src/main/java/br/com/bhl/superfid/model/ItemCarrinho.java
@@ -5,10 +5,10 @@ import java.io.Serializable;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -24,8 +24,8 @@ public class ItemCarrinho implements Serializable{
 	@Column(name = "codigoItem")
 	private Long codigoItem;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@Column(name="codigoCarrinho")
+	@ManyToOne
+	@JoinColumn(name="codigoCarrinho")
 	private Carrinho carrinho;
 	
 	@OneToOne(cascade = CascadeType.ALL)
